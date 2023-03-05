85f0bda18176fda6677cbc1c3dd8db33d8b1635e
lucastusch <lucas.tusch@gmail.com>
2023-03-05 12:50:43 +0100

    now everything ready for database interaction
    note:
    # made a detailed description-comment, where and what should be done ("# todo:")

--- src/gui/login_register_gui.py
+++ src/gui/login_register_gui.py
@@ -1,6 +1,6 @@
 from functools import partial
 
-from PyQt6.QtWidgets import QWidget, QMainWindow, QHBoxLayout, QLabel, QLineEdit, QVBoxLayout, QPushButton
+from PyQt6.QtWidgets import QWidget, QMainWindow, QHBoxLayout, QLabel, QLineEdit, QVBoxLayout, QPushButton, QMessageBox
 
 FORM_FIELDS: list[str] = [
     "Username",
@@ -45,6 +45,8 @@ class Fields(QHBoxLayout):
         self.login: list[[str, QLabel, QLineEdit or None]] = []
         self.register: list[[str, QLabel, QLineEdit or None]] = []
 
+        self.user_data: list[[str, str]] = []
+
         self.parent.layout.addLayout(self.layout)
 
     def generate_general_form(self, _type):
@@ -57,15 +59,15 @@ class Fields(QHBoxLayout):
             line = QLineEdit()
             line.setEchoMode(QLineEdit.EchoMode.Password) if ele == "Password" or ele == "Confirm password" else None
 
-            if _type == "Login":
-                self.login.append([ele, label, line])
+            if _type == "Register":
+                self.register.append([ele, label, line])
 
                 layout.addWidget(label)
                 layout.addWidget(line)
 
-            if _type == "Register":
+            if _type == "Login":
                 if ele != "Confirm password":
-                    self.register.append([ele, label, line])
+                    self.login.append([ele, label, line])
                     layout.addWidget(label)
                     layout.addWidget(line)
 
@@ -74,7 +76,7 @@ class Fields(QHBoxLayout):
                         label_spacer = QLabel()
                         label_spacer.setStyleSheet("font-size: 16px")
 
-                        self.register.append(["", label_spacer])
+                        self.login.append(["", label_spacer])
                         layout.addWidget(label_spacer)
 
         self.layout.addLayout(layout)
@@ -94,6 +96,63 @@ class Fields(QHBoxLayout):
             self.login.clear()
             self.register_ui()
 
+    def display_alert(self, check: bool, message: str):
+        alert = QMessageBox()
+
+        if check:
+            alert.setIcon(QMessageBox.Icon.Information)
+            alert.setStandardButtons(QMessageBox.StandardButton.Ok)
+            alert.setWindowTitle("Alert")
+            alert.setText(message)
+
+        else:
+            alert.setIcon(QMessageBox.Icon.Warning)
+            alert.setStandardButtons(QMessageBox.StandardButton.Retry)
+            alert.setWindowTitle("Alert")
+            alert.setText(message)
+
+        alert.exec()
+
+    def check_field(self):
+        self.user_data.clear()
+
+        if self.login:
+            for ele in self.login:
+                if len(ele) == 3:
+                    if not ele[2].text():
+                        self.display_alert(False, f"Field: {ele[1].text()} is missing a parameter")
+                        self.user_data.clear()
+
+                    else:
+                        self.user_data.append([ele[1].text(), ele[2].text()])
+
+                        # todo: here we have to check, if the user is in the database
+                        # todo: if he is in the database: load the next page (where the user can join a lobby) - else
+                        #  throw alert
+
+        if self.register:
+            for ele in self.register:
+                if len(ele) == 3:
+                    if not ele[2].text():
+                        self.display_alert(False, f"Field: {ele[1].text()} is missing a parameter")
+                        self.user_data.clear()
+
+                    else:
+                        self.user_data.append([ele[1].text(), ele[2].text()])
+
+            for i in range(len(self.user_data)):
+                if self.user_data[i][0] == "Confirm password":
+                    if self.user_data[i - 1][1] != self.user_data[i][1]:
+                        self.display_alert(False, f"Passwords don't match")
+                        self.user_data.clear()
+
+                    else:
+                        # todo: here we have to put the user in the database
+                        # todo: if user is already in database: throw alert - else put user in database
+                        pass
+
+        print(self.user_data)
+
     def login_ui(self):
         self.generate_general_form("Login")
 
@@ -109,6 +168,9 @@ class Fields(QHBoxLayout):
             if ele == "Register":
                 button.setStyleSheet("background-color: lightblue; font-size: 16px")
                 button.clicked.connect(partial(self.clear_field, "Register"))
+
+            if ele == "Login":
+                button.clicked.connect(partial(self.check_field))
 
             layout.addWidget(button)
         self.layout.addLayout(layout)
@@ -128,6 +190,9 @@ class Fields(QHBoxLayout):
             if ele == "Login":
                 button.setStyleSheet("background-color: lightblue; font-size: 16px")
                 button.clicked.connect(partial(self.clear_field, "Login"))
+
+            if ele == "Register":
+                button.clicked.connect(partial(self.check_field))
 
             layout.addWidget(button)
         self.layout.addLayout(layout)
